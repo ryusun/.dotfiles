ea971980e12f8ad3c0acbca5373f9e6018ac266f
yuji hori <yuji.hori@gmail.com>
2013-12-24 05:55:04 +0000

    add comments

--- enviroments.sh
+++ enviroments.sh
@@ -8,13 +8,15 @@ sudo yum install zsh -y
 
 git submodule update --init
 
-
+echo "add ~/.zshrc"
 [ -e ~/.zshrc ] && sudo rm -f ~/.zshrc
 ln -s ~/.dotfiles/zsh/.zshrc ~/.zshrc
 
+echo "add ~/.vimrc"
 [ -e ~/.vimrc ] && sudo rm -f ~/.vimrc
 ln -s ~/.dotfiles/vim/.vimrc ~/.vimrc
 
+echo "add ~/.vim/bundle/neobundle"
 [ -d ~/.vim ] || mkdir -p ~/.vim/bundle
 ln -s ~/.dotfiles/vim/bundle/neobundle ~/.vim/bundle/neobundle
 
